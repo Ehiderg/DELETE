0673c7357c13e5c87cedff436222307ef27c7a49
Ehiderg <ehiderg@uninorte.edu.co>
2023-11-25 00:38:05 -0500

    Ya borra el archivo imagen relacionada al registro

--- delete.py
+++ delete.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 import pyodbc
 from datetime import datetime
-import requests
+import os
 
 
 app = Flask(__name__)
@@ -22,16 +22,20 @@ def agregar_log(cedula, tipo_documento,operacion, detalles):
 @app.route('/borrar/<numero_documento>', methods=['DELETE'])
 def borrar(numero_documento):
     # Consultar en el microservicio de consulta para verificar la existencia
-    consulta_response = requests.get(f'http://localhost:5000/consultar/{numero_documento}')
-    
-    if consulta_response.status_code == 200:
-        row = consulta_response.json()
+    consulta_response = cursor.execute("SELECT * FROM Registro WHERE NumeroDocumento=?", numero_documento)
+    row = consulta_response.fetchone()
+    if row:
+        
+        if os.path.exists(row.Foto):
+            os.remove(row.Foto)
+
+        # Agregar operación al log
+        agregar_log(numero_documento, row.TipoDocumento, 'Borrado', f"Se eliminó la información de {row.PrimerNombre} {row.Apellidos} con número de documento {numero_documento}")
+
         # Persona encontrada, proceder con el borrado
         cursor.execute("DELETE FROM Registro WHERE NumeroDocumento=?", numero_documento)
         conn.commit()
-
-        # Agregar operación al log
-        agregar_log(numero_documento, row['TipoDocumento'], 'Borrado', f"Se eliminó la información de {row['PrimerNombre']} {row['Apellidos']} con número de documento {numero_documento}")
+        
 
         return jsonify({"mensaje": "Borrado exitoso"}), 200
     else:
